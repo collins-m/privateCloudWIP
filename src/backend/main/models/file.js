3822a354ecf0f6d3606735240e38893118863d3b
collim38 <michael.collins38@mail.dcu.ie>
2020-03-09 18:24:03 +0000

    SAVEPOINT

--- src/backend/main/models/file.js
+++ src/backend/main/models/file.js
@@ -4,8 +4,54 @@ const config = require('../config/database');
 
 // file schema
 const FileSchema = mongoose.Schema({
-    
+    originalFilename: {
+        type: String,
+        required: true
+    },
+    filename: {
+        type: String,
+        required: true
+    },
+    path: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: String,
+        required: true
+    }
 });
 
 // export schema
 const File = module.exports = mongoose.model('File', FileSchema);
+
+/**
+* [find file by ID]
+* @param {[String]} id [ID pertaining to file]
+* @return {[JSON]} [file object]
+*/
+module.exports.getFileById = function(id, callback){
+    File.findById(id, callback);
+}
+
+/**
+* [find file by user and name]
+* @param {[String]} user [user pertaining to file]
+* @return {[JSON]} [file object]
+*/
+module.exports.getFileByName = function(user, originalFilename, callback){
+    const query = {
+        user: user,
+        originalFilename: originalFilename
+    }
+    User.findOne(query, callback);
+}
+
+/**
+* [add a new file]
+* @param {[User]} newFile [File object as per above schema]
+* @return {[JSON]} [success/failure]
+*/
+module.exports.addFile = function(newFile, callback){
+    newFile.save(callback);
+}
--- src/backend/main/routes/api/file/upload.js
+++ src/backend/main/routes/api/file/upload.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const multer = require('multer');
 
+const File = require('../../../models/file');
+
 const router = express.Router();
 
 var storage = multer.diskStorage({
@@ -15,9 +17,23 @@ var storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-router.post('/upload', upload.single('file'), (req, res, next) => {
-    // store reference to file in db
-    return res.send(req.file);
-})
+router.post('/upload', passport.authenticate('jwt', {session:false}), upload.single('file'), (req, res, next) => {
+    // cast data as File object
+    let newFile = new File({
+        originalFilename: req.file.originalname,
+        filename: req.file.filename,
+        path: req.file.path,
+        owner: req.owner
+    });
+
+    //attempt to add file
+    File.addFile(newFile, (err, file) => {
+        if (err) {
+            return res
+                .status(400)
+                .json({success: false, msg: 'Failed to upload file'})
+        }
+    })
+});
 
 module.exports = router;
