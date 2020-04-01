e40142b76b1d1b1014ea3077908c9dd9a0d20bbb
collim38 <michael.collins38@mail.dcu.ie>
2020-04-01 19:27:26 +0100

    SAVEPOINT

--- src/backend/main/routes/api/file/upload.js
+++ src/backend/main/routes/api/file/upload.js
@@ -8,46 +8,72 @@ const File = require('../../../models/file');
 
 const router = express.Router();
 
-var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, './public/');
-    },
-    filename: (req, file, cb) => {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+// define disk storage instructions
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, './public/')
+  },
+  filename: (req, file, callback) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    callback(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+const upload = multer({ storage: storage })
+
+// define move operation for files
+moveFile = function(oldPath, newPath, callback) {
+    fs.rename(oldPath, newPath, function (err) {
+        if (err) {
+            if (err.code === 'EXDEV') {
+                copy();
+            } else {
+                callback(err);
+            }
+            return;
+        }
+        callback();
+    });
+
+    function copy() {
+        var readStream = fs.createReadStream(oldPath);
+        var writeStream = fs.createWriteStream(newPath);
+
+        readStream.on('error', callback);
+        writeStream.on('error', callback);
+
+        readStream.on('close', function () {
+            fs.unlink(oldPath, callback);
+        });
+
+        readStream.pipe(writeStream);
     }
-});
-
-const upload = multer({ storage: storage });
-
-// function to put files in correct subdirectories
-const uploadFile = function(owner) {
-//     if (!fs.existsSync('./public/' + owner)){
-//         fs.mkdirSync('./pubic/' + owner);
-//     }
-    // set as destination
-    upload.single('file');
-
 }
 
-
-router.post('/upload', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+router.post('/upload', passport.authenticate('jwt', {session:false}), upload.single('file'), (req, res, next) => {
     // cast data as File object
     let newFile = new File({
         originalFilename: req.file.originalname,
         filename: req.file.filename,
-        path: req.file.path,
-        owner: req.owner
+        path: 'public/' + req.body.owner + '/' + req.file.filename,
+        owner: req.body.owner
     });
 
-    //attempt to add file
-    uploadFile(req.owner);
+    // move file to user subdirectory
+    moveFile(req.file.path, newFile.path, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
 
     File.addFile(newFile, (err, file) => {
         if (err) {
             return res
                 .status(400)
-                .json({success: false, msg: 'Failed to upload file'})
+                .json({success: false, msg: 'Failed to upload file'});
         }
+        return res
+            .status(201)
+            .json({success: true, msg: 'Successfully uploaded file'});
     })
 });
 
